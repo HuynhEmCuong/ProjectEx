5f739fe7f267b3d70f05f2d7183988e2380a3056
huynhemcuong <huynhcuongem7597@gmail.com>
2021-01-24 19:44:21 +0700

    # commit

--- Web-API/Web-API/Controllers/Usercontroller.cs
+++ Web-API/Web-API/Controllers/Usercontroller.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Web_API.Dtos;
 using Web_API.Helpers.Common;
 using Web_API.Routes;
 using Web_API.Service.Interface;
@@ -23,6 +24,10 @@ namespace Web_API.Controllers
         public async Task<IActionResult> GetAllUser() => Ok(await _service.GetALl());
 
         [HttpGet, Route(ApiRouter.User.SerachUser)]
-        public async Task<IActionResult> SearchUser([FromQuery]PaginationParams page, string keyword) => Ok(await _service.Search(keyword,page));
+        public async Task<IActionResult> SearchUser([FromQuery] PaginationParams page, string keyword) => Ok(await _service.Search(keyword, page));
+
+        [HttpPost, Route(ApiRouter.User.AddUser)]
+        public async Task<IActionResult> AddUser(UserDto user) => Ok(await _service.Add(user));
+
     }
 }
